e0825be0dc0afcdf9ea6b846a7682b392830085b
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2024-04-02 11:40:12 +0900

    0402 3 class add problem

---
+++ src/App16.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+public class App16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k, m, row, n = 0;
+        System.out.print("층수를 입력하세요: ");
+        row = scanner.nextInt();
+        for(int i = 0;i <row;i++){
+            for(int j = row-1-i;j>0;j--){
+                System.out.print("*");
+            }
+            for(m=0;m<(2*i)+1;m++){
+                System.out.print(n);
+                n++;
+                if(n>=10)
+                    n=0;
+            }
+            for(int j = row-1-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
